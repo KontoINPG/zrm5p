f4c236d9152d42edcf13ae997fb594f2168173b3
KontoInpg <kontoinpg@gmail.com>
2020-05-08 19:32:00 +0200

    .

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dmw/zrm5p/src
-# Build directory: /home/dmw/zrm5p/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
--- devel/share/common-lisp/ros/learning_actionlib/msg/_package_AveragingResult.lisp
+++
@@ -1,6 +0,0 @@
-(cl:in-package learning_actionlib-msg)
-(cl:export '(MEAN-VAL
-          MEAN
-          STD_DEV-VAL
-          STD_DEV
-))
